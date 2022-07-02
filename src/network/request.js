e7e520f10cf562b7e2f95dee156a5f3deb2bae2a
DDZ-2020 <2264241026@qq.com>
2022-07-02 11:38:29 +0800

    封装的网络请求
    更改了baseURL

--- src/network/request.js
+++ src/network/request.js
@@ -34,11 +34,17 @@ import { resolve } from 'core-js/fn/promise';
 export function request(config) {
     // 1.创建axios的实例
     const instance = axios.create({
-        baseURL: 'http://123.207.32.32:8000',
+        baseURL: 'http://152.136.185.210:7878/api/hy66',
         timeout: 5000
     });
 
     // 2.axios的拦截器
+    // 响应拦截
+    instance.interceptors.response.use(response => {
+        return response.data;
+    }, err => {
+        return err;
+    });
 
 
     // 发送真正的网络请求，返回本省就是一个promise
