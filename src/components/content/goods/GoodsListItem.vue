e729c44f705359e697b0371c02a35c5690d08f80
DDZ-2020 <2264241026@qq.com>
2022-07-02 11:37:07 +0800

    商品列表与TabControl
    [流行，新款，精选]

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,74 @@
+<template>
+    <div class="goods-item">
+        <img :src="goodsItem.show.img" alt="">
+        <div class="info">
+            <p>{{ goodsItem.title }}</p>
+            <span class="price">{{ goodsItem.price }}</span>
+            <span class="collect">{{ goodsItem.cfav }}</span>
+        </div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'GoodsListItem',
+    props: {
+        goodsItem: {
+            type: Object,
+            default() {
+                return {};
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.goods-item {
+    width: 48%;
+
+    padding-bottom: 40px;
+    position: relative;
+    font-size: 12px;
+}
+
+.goods-item img {
+    border-radius: 5px;
+    width: 100%;
+}
+
+.info {
+    position: absolute;
+    bottom: 5px;
+    left: 0;
+    right: 0;
+    text-align: center;
+}
+
+.info p {
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+    margin-bottom: 3px;
+}
+
+.price {
+    margin-right: 20px;
+    color: var(--color-high-text);
+}
+
+.collect {
+    position: relative;
+}
+
+.collect::before {
+    content: '';
+    position: absolute;
+    left: -16px;
+    top: 0px;
+    width: 14px;
+    height: 14px;
+    background: url("~assets/img/common/collect.svg") 0 0/14px 14px;
+}
+</style>
---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="tab-control">
+        <ul>
+            <li v-for="(item, index) in titles" @click="itemClick(index)" :class="{ active: index === currentIndex }">
+                <span>{{ item }}</span>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TabControl',
+    props: {
+        titles: {
+            type: Array,
+            default() {
+                return [];
+            }
+        }
+    },
+
+    data() {
+        return {
+            currentIndex: 0,
+        }
+    },
+
+    methods: {
+        itemClick(index) {
+            this.currentIndex = index;
+            this.$emit('tabClick', index);
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+.tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+    background-color: #fff;
+}
+
+.tab-control ul {
+    display: flex;
+    text-align: center;
+    height: 40px;
+    line-height: 40px;
+}
+
+.tab-control ul li {
+    flex: 1;
+}
+
+.tab-control ul li span {
+    padding: 0 10px 5px;
+}
+
+.active span {
+    border-bottom: 3px solid var(--color-high-text);
+    color: var(--color-high-text);
+}
+</style>
